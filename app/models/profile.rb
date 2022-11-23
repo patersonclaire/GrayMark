e776bd9007c30dca3ea2e2c949d0cca475f2db85
Alden Wahsono <114331474+Boorrito15@users.noreply.github.com>
2022-11-23 17:10:34 +0800

    Update profile.rb

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ApplicationRecord
   belongs_to :school
-  has_many :profile_alleries
-  has_many :ingredients, through: :profile_alleriess
+  has_many :profile_allergies
+  has_many :ingredients, through: :profile_allergies
 end
