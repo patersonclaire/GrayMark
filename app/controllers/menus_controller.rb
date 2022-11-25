8b619ab52b59cb8d30023a603cd1d697f71cd677
Alden Wahsono <aldenwahsono15@icloud.com>
2022-11-25 16:05:01 +0800

    Fix: controllers

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,6 +1,16 @@
 class MenusController < ApplicationController
+  before_action :set_menu, only: [:show]
 
   def index
     @menus = Menu.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_school
+    @menu = Menu.find(params[:id])
   end
 end
