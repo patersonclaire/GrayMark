86136d7a8294092a2eb12812d23882eb9e74bf32
Alden Wahsono <aldenwahsono15@icloud.com>
2022-11-28 15:15:47 +0800

    school_menu_controller

--- app/controllers/school_menus_controller.rb
+++ app/controllers/school_menus_controller.rb
@@ -1,10 +1,28 @@
 class SchoolMenusController < ApplicationController
   def index
-    @menus = Menu.all
+    @school_menus = Menu.all
   end
 
   def show
     @school_menu = SchoolMenu.find(params[:id])
     @menus_in_week = @school_menu.menus.where(menu_date: @school_menu.date..(@school_menu.date + 5.days)).limit(5)
+  end
+
+  def new
+    @school_menu = SchoolMenu.new
+  end
+
+  def create
+    @school_menu = SchoolMenu.new(school_menu_params)
+  end
+
+  private
+
+  def set_school
+    @school_menu = SchoolMenu.find(params[:id])
+  end
+
+  def school_menu_params
+    params.require(:date)
   end
 end
