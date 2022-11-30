a75f01fa7091b36068c171525b141c2a7e246c10
Alden Wahsono <aldenwahsono15@icloud.com>
2022-11-30 12:49:21 +0800

    seeds are done

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,3 +1,6 @@
+require "json"
+require "open-uri"
+
 class MenusController < ApplicationController
   def new
     @menu = Menu.new
@@ -29,7 +32,6 @@ class MenusController < ApplicationController
       end
 
       (@school_menu.date..(@school_menu.date + 4.days)).each do |date|
-
         @menu = Menu.new
         @menu.school_menu = @school_menu
         @menu.profile = profile
@@ -37,11 +39,35 @@ class MenusController < ApplicationController
         @menu.save
 
         @school_menu.dishes.each do |dish|
-          unless dish.ingredient_ids.any? { |id| allergens.include?(id) }
+          if dish.ingredient_ids.any? { |id| allergens.include?(id) }
+            # find a substitute recipe -> search_recipes(...)
+          else
             DayDish.create(menu: @menu, dish: dish)
           end
         end
       end
     end
   end
+
+  private
+
+  def search_recipes(diet, intolerances, excludeIngredients, mealtype)
+
+    #diet is selected one by one hard seeded
+    #intolerances is selected one by one hard seeded
+    #excludeIngredients is selected one by one hard seeded
+    #Repeat this for each meal type (main, side, dessert)
+
+    url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=#{ENV['SPOONACULAR_API']}&diet=#{diet}&intolerances=#{intolerances}&excludeIngredients=#{excludeIngredients}&mealtype#{mealtype}"
+    dish_serialized = URI.open(url).read
+    dishes = JSON.parse(dish_serialized)
+    dish = dishes["results"].first["title"]
+
+    # dishes.results.each do |dish|
+    # dish.title
+    # end
+    # https://api.spoonacular.com/recipes/complexSearch?apiKey=99c01005a66940b5a1c9b6e6cfed1ef7&excludeIngredients=eggs,bread,brown rice&diet=vegetarian
+  end
+
+  search_recipes("vegetarian", "Seafood", "", "main course")
 end
