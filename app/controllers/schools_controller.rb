7cdce5530945a0f51411a4627ff03b393baad315
Alden Wahsono <aldenwahsono15@icloud.com>
2022-12-26 04:01:54 +0700

    Fixed & refactored school controller show

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -6,17 +6,21 @@ class SchoolsController < ApplicationController
   end
 
   def show
-    # raise
-    @school_menus = @school.school_menus.where("date >= ?", Date.today).order(:date)
+    # you can think of date+18 as end date
+    # what about menus for the next term they make in advance - that shouldn't be in current menu - maybe an upcoming section
+    @current_menus = @school.school_menus.where("date+18 >= ?", Date.today)
+    @archived_menus = @school.school_menus.where("date+18 < ?", Date.today)
 
-    @first_school_menu = @school_menus.first
-    @first_week_menus = @first_school_menu.menus.where("EXTRACT(dow from menu_date) = ?", Date.current.monday.wday).where(menus: { menu_date: @first_school_menu.date })
+    # @archived_menus = @school.school_menus.where("date < ?", Date.today).order(:date)
 
-    @second_school_menu = @school_menus.second
-    @second_week_menus = @second_school_menu.menus.where("EXTRACT(dow from menu_date) = ?", Date.current.monday.wday).where(menus: { menu_date: @second_school_menu.date })
+    # @first_school_menu = @school_menus.first
+    # @first_week_menus = @first_school_menu.menus.where("EXTRACT(dow from menu_date) = ?", Date.current.monday.wday).where(menus: { menu_date: @first_school_menu.date })
 
-    @third_school_menu = @school_menus.third
-    @third_week_menus = @third_school_menu.menus.where("EXTRACT(dow from menu_date) = ?", Date.current.monday.wday).where(menus: { menu_date: @third_school_menu.date })
+    # @second_school_menu = @school_menus.second
+    # @second_week_menus = @second_school_menu.menus.where("EXTRACT(dow from menu_date) = ?", Date.current.monday.wday).where(menus: { menu_date: @second_school_menu.date })
+
+    # @third_school_menu = @school_menus.third
+    # @third_week_menus = @third_school_menu.menus.where("EXTRACT(dow from menu_date) = ?", Date.current.monday.wday).where(menus: { menu_date: @third_school_menu.date })
   end
 
   private
@@ -24,5 +28,5 @@ class SchoolsController < ApplicationController
   def set_school
     @school = School.find(params[:id])
   end
-  
+
 end
